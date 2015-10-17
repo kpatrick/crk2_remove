8a8fc300900e04050fc4b7125ddaf430addfef8a
Kyle Patrick <kyle_patrick@yahoo.com>
2015-10-16 18:18:07 -0700

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151016055326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "children", force: true do |t|
     t.string   "code"
     t.integer  "family_id"
