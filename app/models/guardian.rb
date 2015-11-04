2dce4361edd196d23424c2fd8ef060ee56c66416
kpatrick <kyle_patrick@yahoo.com>
2015-11-03 20:23:30 -0800

    add more guardian types

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -1,9 +1,14 @@
 class Guardian < ActiveRecord::Base
   belongs_to :family
-  RELATIONSHIPS_ARRAY = [["Mother", "mother"],
+  RELATIONSHIPS_ARRAY = [
+      ["Mother", "mother"],
       ["Father", "father"],
       ["Step Mother", "step_mother"],
+      ["Step Father", "step_father"],
       ["Grand Mother", "grand_mother"],
-      ["Older Sibling", "older_sibling"]]
+      ["Grand Father", "grand_father"],
+      ["Older Sibling", "older_sibling"],
+      ["Other", "other"]
+      ]
   RELATIONSHIPS_MAP = Hash[RELATIONSHIPS_ARRAY.map{|a| [a[1], a[0]]}]
 end
